96bcf129c8d8cb07e703ab1329931810b1ff2cf8
Anis Safine <anis@safine.me>
2014-03-10 21:20:24 +0100

    Integration of 'article'

---
+++ article.php
@@ -0,0 +1,99 @@
+<?php theme_include('header'); ?>
+
+<section class="posts" id="article-<?php echo article_id(); ?>">
+    <article>
+        <header>
+            <figure>
+                <a href="<?php echo article_url(); ?>">
+                    <img src="<?php echo article_custom_field('thumbnail'); ?>" />
+                </a>
+
+                <figcaption>
+                    <h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
+                    <p>
+                        Publi&eacute; le
+                        <time datetime="<?php echo date('c', article_time()); ?>" pubdate>
+                            <?php
+                                $months = array(
+                                    'January' => 'Janvier',
+                                    'February' => 'Février',
+                                    'March' => 'Mars',
+                                    'April' => 'Avril',
+                                    'May' => 'Mai',
+                                    'June' => 'Juin',
+                                    'July' => 'Juillet',
+                                    'August' => 'Août',
+                                    'September' => 'Septembre',
+                                    'October' => 'Octobre',
+                                    'November' => 'Novembre',
+                                    'December' => 'Décembre'
+                                );
+
+                                echo strtr(date('d F Y à H:i', article_time()), $months);
+                            ?>
+                        </time>
+                        par <a href="#" rel="author"><?php echo article_author(); ?></a>.
+                    </p>
+                    <?php if(comments_open()): ?>
+                    <p class="comments"><a href="#comments"><span><?php echo total_comments(); ?></span> <?php echo pluralise(total_comments(), 'commentaire'); ?></a></p>
+                    <?php endif; ?>
+                </figcaption>
+            </figure>
+        </header>
+
+        <div class="content">
+            <?php echo article_markdown(); ?>
+        </div>
+    </article>
+</section>
+
+<section class="comments" id="comments">
+    <?php if(has_comments()): ?>
+        <?php $i = 0; while(comments()): $i++; ?>
+        <article id="comment-<?php echo comment_id(); ?>">
+            <figure>
+                <img src="<?php echo comment_gravatar(); ?>" />
+            </figure>
+
+            <header>
+                <time><?php echo relative_time(comment_time()); ?></time>
+                <h1>Rédigé par <em><?php echo comment_name(); ?></em></h1>
+            </header>
+
+            <div class="content"><?php echo nl2br(comment_text()); ?></div>
+            <div class="clearer"></div>
+        </article>
+        <?php endwhile; ?>
+    <?php elseif(comments_open()): ?>
+    <p>Sois le premier &agrave; commenter cet article, yo !</p>
+    <?php else: ?>
+    <p class="no-comments">Les commentaires ont &eacute;t&eacute; d&eacute;sactiv&eacute;s sur cet article.</p>
+    <?php endif; ?>
+
+    <?php if(comments_open()): ?>
+    <form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
+        <?php echo comment_form_notifications(); ?>
+
+        <p class="name">
+            <label for="name">Ton petit nom :</label>
+            <?php echo comment_form_input_name('placeholder="Ton petit nom"'); ?>
+        </p>
+
+        <p class="email">
+            <label for="email">Ton courriel :</label>
+            <?php echo comment_form_input_email('placeholder="Ton courriel (ne sera ni publié ni utilisé à des fins commerciales)"'); ?>
+        </p>
+
+        <p class="textarea">
+            <label for="text">Ton mot doux :</label>
+            <?php echo comment_form_input_text('placeholder="Ton mot doux"'); ?>
+        </p>
+
+        <p class="submit">
+            <?php echo comment_form_button('Soumettre ton commentaire'); ?>
+        </p>
+    </form>
+    <?php endif; ?>
+</section>
+
+<?php theme_include('footer'); ?>
--- functions.php
+++ functions.php
@@ -5,6 +5,41 @@ function pluralise($amount, $str, $alt = '') {
         : $str . ($alt !== '' ? $alt : 's');
 }
 
+function relative_time($date) {
+    if(is_numeric($date)) $date = '@' . $date;
+
+    $user_timezone = new DateTimeZone(Config::app('timezone'));
+    $date = new DateTime($date, $user_timezone);
+
+    // get current date in user timezone
+    $now = new DateTime('now', $user_timezone);
+
+    $elapsed = $now->format('U') - $date->format('U');
+
+    if($elapsed <= 1) {
+        return 'A l\'instant';
+    }
+
+    $times = array(
+        31104000 => 'année',
+        2592000 => 'mois',
+        604800 => 'semaine',
+        86400 => 'jour',
+        3600 => 'heure',
+        60 => 'minute',
+        1 => 'seconde'
+    );
+
+    foreach($times as $seconds => $title) {
+        $rounded = $elapsed / $seconds;
+
+        if($rounded > 1) {
+            $rounded = round($rounded);
+            return 'Il y a ' . $rounded . ' ' . pluralise($rounded, $title);
+        }
+    }
+}
+
 function twitter_account() {
 	return site_meta('twitter');
 }
@@ -19,4 +54,8 @@ function github_account() {
 
 function github_url() {
 	return 'https://github.com/' . github_account();
+}
+
+function comment_gravatar() {
+    return 'http://www.gravatar.com/avatar/' . md5( strtolower( trim( comment_email() ) ) ) . '?s=80';
 }
